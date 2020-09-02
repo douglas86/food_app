b037e2658169ad6b84c1886cf4487117382bf4f8
douglas86 <douglasmaxton@gmail.com>
2020-09-02 22:23:34 +0200

    Created the the detail.html page

--- food/urls.py
+++ food/urls.py
@@ -1,4 +1,11 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [path("", views.index, name="index")]
+app_name = "food"
+urlpatterns = [
+    # home
+    path("", views.index, name="index"),
+    # /item_id number
+    path("<int:item_id>/", views.detail, name="detail"),
+]
+
--- food/views.py
+++ food/views.py
@@ -6,5 +6,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    context = {'item_list':item_list}
+    context = {"item_list": item_list}
     return render(request, "food/index.html", context)
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        "item": item,
+    }
+    return render(request, "food/detail.html", context)
