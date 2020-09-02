d16f761f917d80a29f2ad0715835a6ad52cf25d0
douglas86 <douglasmaxton@gmail.com>
2020-09-02 21:46:47 +0200

    Designed the home page layout

--- food/views.py
+++ food/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    item_objects = Item.objects.all()
-    template = loader.get_template("food/index.html")
-    context = {}
-    return HttpResponse(template.render(context, request))
+    item_list = Item.objects.all()
+    context = {'item_list':item_list}
+    return render(request, "food/index.html", context)
