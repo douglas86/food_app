b1f65a2df9d3c04f9a49c68e7679c5831c0433d6
douglas86 <douglasmaxton@gmail.com>
2020-09-02 19:35:33 +0200

    Added navbar with ham icon

--- food/models.py
+++ food/models.py
@@ -9,3 +9,5 @@ class Item(models.Model):
         max_length=500,
         default="https://i.pinimg.com/originals/bb/95/1f/bb951f5407e8b8bb6075d3eee24d3bcd.jpg",
     )
+    def __str__(self):
+        return self.item_name
