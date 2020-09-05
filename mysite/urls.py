4c26f22176e8826e6dc317cd535f60c3ca2e62ba
douglas86 <douglasmaxton@gmail.com>
2020-09-05 22:26:40 +0200

    Adding the user profile picture

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -34,3 +36,8 @@ urlpatterns = [
     ),
     path("profile/", user_views.profilepage, name="profile"),
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
