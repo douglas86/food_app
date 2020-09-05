3b76f489d6d4dbfb3d2b9164ae56a1b657be5f10
douglas86 <douglasmaxton@gmail.com>
2020-09-05 11:13:16 +0200

    registering a success message

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -9,6 +10,13 @@ def register(request):
     :returns: TODO
 
     """
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Welcome {username}, your account is created')
+            return redirect("food:index")
+    else:
+        form = UserCreationForm()
 
     return render(request, 'user/register.html', {'form':form})
