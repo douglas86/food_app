fc5d9bc50ac6875244905c9df8ae03b82c629470
douglas86 <douglasmaxton@gmail.com>
2020-09-06 12:22:10 +0200

    add user to post

--- food/models.py
+++ food/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
@@ -9,5 +11,6 @@ class Item(models.Model):
         max_length=500,
         default="https://i.pinimg.com/originals/bb/95/1f/bb951f5407e8b8bb6075d3eee24d3bcd.jpg",
     )
+
     def __str__(self):
         return self.item_name
