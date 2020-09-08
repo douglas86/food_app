8f7f10dec31b3ab8f151e965aebdfec9a49f7fdd
douglas86 <douglasmaxton@gmail.com>
2020-09-08 13:01:21 +0200

    updating heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY =  'f8=58k2a70x-^sjhi=s9501^!fc7u)pwl9xf1hg2v=l0sdrcp#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = FALSE
 
 ALLOWED_HOSTS = ["food-app-345.herokuapp.com/", "127.0.0.1" ]
 #  ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
